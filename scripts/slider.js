a61a41fc3f19a541e61adfddc9c3eb9e9bf97afc
Kirill Vanykov <kiryshavanykov@gmail.com>
2018-08-04 13:58:38 +0200

    first commit

---
+++ scripts/slider.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+});
